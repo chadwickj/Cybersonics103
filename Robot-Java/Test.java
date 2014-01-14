fb34c18b1bf13f83af3db39c2310e92c9b830775
Mnenmenth <iamrazim12@gmail.com>
2014-01-14 16:33:16 -0500

    Found How to Set Up Java for Robot

--- Robot-Java/Test.java
+++ Robot-Java/Test.java
@@ -1,4 +1,5 @@
 /*
 http://first.wpi.edu/Images/CMS/First/Getting_Started_with_Java_for_FRC.pdf
 http://www.usfirst.org/uploadedFiles/Robotics_Programs/FRC/Game_and_Season__Info/2011_Assets/Kit_of_Parts/How_to_Build_and_Load_Programming_in_LabVIEW,_C,_and_Java.pdf
+
 */
