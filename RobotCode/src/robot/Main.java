6323cafc3809c5283e9ccf1071d6b0ce6ff774b0
Razim Zavac <Razim Zavac@10.1.122.177>
2014-02-01 13:10:57 -0500

    Added to Shooter Class/Added Robot Builder.jar into folder

--- RobotCode/src/robot/Main.java
+++ RobotCode/src/robot/Main.java
@@ -12,12 +12,4 @@ public class Main extends SimpleRobot {
     public Joystick leftControlStick = new Joystick(3);
     public Joystick rightControlStick = new Joystick(4);
     
-    public void doSomething(){
-        chassis.drive(-0.5,0.0);
-        Timer.delay(2.0);
-        //or use this
-        //Thread.sleep(2000);
-        chassis.drive(0.0,0.0);
-    }
-    
 }
